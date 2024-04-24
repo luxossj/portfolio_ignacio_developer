25d78423aa6c49df4ee0a65c5804719b9c6339c9
Luis <luisignaciocontacto@gmail.com>
2024-04-24 19:30:05 -0400

    test

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -15,7 +15,7 @@ from os import path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+# test para ver main
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
