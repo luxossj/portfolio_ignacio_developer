9f47ea5eb0dbac791e014623eb2c48137e350bc4
Luis <luisignaciocontacto@gmail.com>
2024-04-25 11:02:51 -0400

    test host

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -31,11 +31,11 @@ DEBUG = 'RENDER' not in os.environ
 
 
 # https://docs.djangoproject.com/en/3.0/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['portfolio-ignacio-developer.onrender.com','ignaciodeveloper.com']
 
-RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
-if RENDER_EXTERNAL_HOSTNAME:
-    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+# RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+# if RENDER_EXTERNAL_HOSTNAME:
+#     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
