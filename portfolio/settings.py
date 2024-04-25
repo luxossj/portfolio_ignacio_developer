9c94e804b7fb63d3c8607b3b3fb7906d9b34e99d
Luis <luisignaciocontacto@gmail.com>
2024-04-24 23:11:50 -0400

    settings

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -93,7 +93,7 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
-        default='postgresql://postgres:postgres@localhost/postgres',
+        default='postgresql://postgres:postgres@localhost:5432/portfolio_ignacio',
         conn_max_age=600
     )
 }
