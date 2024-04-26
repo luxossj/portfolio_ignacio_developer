5d2272c690b4109e23edae6666826160f576b556
Luis <luisignaciocontacto@gmail.com>
2024-04-26 15:36:16 -0400

    deploy push

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -51,13 +51,14 @@ if RENDER_EXTERNAL_HOSTNAME:
 # Application definition
 
 INSTALLED_APPS = [
+    'core'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core'
+
 ]
 
 MIDDLEWARE = [
