3fd106dd428abf122399147babd5e72efbe3ca46
Luis <luisignaciocontacto@gmail.com>
2024-04-25 22:43:32 -0400

    deploy v5

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 # DEBUG = True
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ignaciodeveloper.com']
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
