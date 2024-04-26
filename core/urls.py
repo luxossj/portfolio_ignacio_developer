1966157ef8794dcc77581be8850cccc35e928059
Luis <luisignaciocontacto@gmail.com>
2024-04-25 22:36:10 -0400

    deploy v2

--- core/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import index, projects, contact, about
-
-urlpatterns = [
-    path('', index, name='index'),
-    path('projects', projects, name='projects'),
-    path('contact', contact, name='contact'),
-    path('about', about, name='about'),
-]
