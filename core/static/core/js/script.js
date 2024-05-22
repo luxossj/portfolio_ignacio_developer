aa67f9e52d9b1ad93dbf41b53e3b041531c1d2fe
Luis <luisignaciocontacto@gmail.com>
2024-05-22 13:22:04 -0400

    genesis [3]

--- core/static/core/js/script.js
+++ core/static/core/js/script.js
@@ -48,47 +48,48 @@ document.getElementById("download-file").addEventListener(
   }
 );
 
-    // array with texts to type in typewriter
-    var dataText = [ "Luis Ignacio.", "Full Stack.", "Back-End.", "Front-End.", "Django Expert.","PL-SQL Analylistic.","Angular Design."];
-    
-    // type one text in the typwriter
-    // keeps calling itself until the text is finished
-    function typeWriter(text, i, fnCallback) {
-      // chekc if text isn't finished yet
-      if (i < (text.length)) {
-        // add next character to h1
-       document.querySelector("#titulo").innerHTML = text.substring(0, i+1) +'<spon aria-hidden="true"></spon>';
-  
-        // wait for a while and call this function again for next character
-        setTimeout(function() {
-          typeWriter(text, i + 1, fnCallback)
-        }, 100);
-      }
-      // text finished, call callback if there is a callback function
-      else if (typeof fnCallback == 'function') {
-        // call callback after timeout
-        setTimeout(fnCallback, 700);
-      }
-    }
-    // start a typewriter animation for a text in the dataText array
-     function StartTextAnimation(i) {
-       if (typeof dataText[i] == 'undefined'){
-          setTimeout(function() {
-            StartTextAnimation(0);
-          }, 20000);
-       }
-       // check if dataText[i] exists
-      if (i < dataText[i].length) {
-        // text exists! start typewriter animation
-       typeWriter(dataText[i], 0, function(){
-         // after callback (and whole text has been animated), start next text
-         StartTextAnimation(i + 1);
-       });
-      }
-    }
-    // start the text animation
-    StartTextAnimation(0);
+var dataText = [ 
+  "Web Developer.",
+  "Full Stack.",
+  "Data Analyst.",
+  "Experto en Web.",
+  "Diseñador."
+];
 
+// type one text in the typewriter
+// keeps calling itself until the text is finished
+function typeWriter(text, i, fnCallback) {
+  // check if text isn't finished yet
+  if (i < text.length) {
+    // add next character to h1
+    document.querySelector("#titulo").innerHTML = text.substring(0, i + 1) + '<span aria-hidden="true"></span>';
+
+    // wait for a while and call this function again for next character
+    setTimeout(function() {
+      typeWriter(text, i + 1, fnCallback);
+    }, 100);
+  }
+  // text finished, call callback if there is a callback function
+  else if (typeof fnCallback === 'function') {
+    // call callback after timeout
+    setTimeout(fnCallback, 700);
+  }
+}
+// start a typewriter animation for a text in the dataText array
+function StartTextAnimation(i) {
+  if (typeof dataText[i] === 'undefined') {
+    i = 0; // Reset to start from the first text
+  }
+  if (i < dataText.length) {
+    // text exists! start typewriter animation
+    typeWriter(dataText[i], 0, function() {
+      // after callback (and whole text has been animated), start next text
+      StartTextAnimation(i + 1);
+    });
+  }
+}
+// start the text animation
+StartTextAnimation(0);
 
 
 
@@ -199,21 +200,21 @@ $(document).ready(function() {
     },
     messages: {
       name: {
-        required: "Please enter your name or company"
+        required: "Ingresa tu Nombre o Compañia"
       },
       email: {
-        required: "Please enter your email address",
-        email: "Please enter a valid email address"
+        required: "Ingresa tu Email",
+        email: "Igresa un Email Valido"
       },
       descripcion: {
-        required: "Please enter your message"
+        required: "Escribe tu Mensaje"
       }
     },
     errorPlacement: function(error, element) {
       // Coloca el mensaje de error después del elemento de entrada
       error.insertAfter(element);
       // Aplica el estilo al mensaje de error
-      error.addClass("text-red-700 font-bold");
+      error.addClass("text-left block text-red-700 font-semibold ");
     }
   });
 });
--- core/views.py
+++ core/views.py
@@ -1,7 +1,31 @@
+
+from django.core.mail import send_mail
 from django.shortcuts import render
+from django.http import HttpResponse
+from .forms import ContactForm
 
 def index(request):
-    return render(request,'core/index.html')
+    message = ""
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            descripcion = form.cleaned_data['descripcion']
+            # Envía el correo electrónico
+            send_mail(
+                f'Nuevo mensaje de {name}',  # Asunto
+                descripcion,  # Mensaje
+                email,  # De
+                ['luisignaciocontacto@gmail.com'],  # A
+                fail_silently=False,
+            )
+            message = "Gracias por contactarnos."
+    else:
+        form = ContactForm()
+
+    return render(request, 'core/index.html', {'form': form, 'message': message})
+
 
 def projects(request):
     return render(request, 'core/projects.html')
